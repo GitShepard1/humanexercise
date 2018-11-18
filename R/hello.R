dadab1cf569bf035f460b9a5b8ebc5cad9b5a20e
Alan Ho <uqaho4@uq.edu.au>
2018-11-19 08:57:44 +1000

    Initial commit

---
+++ R/hello.R
@@ -0,0 +1,13 @@
+#' @title
+#'
+#' @description This prints hello
+#' @param n Number of files
+#' @export
+#' @examples \dontrun{
+#' hello()
+#' }
+
+
+hello <- function() {
+  print("Hello, world!")
+}

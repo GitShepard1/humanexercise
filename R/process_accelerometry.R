43cfc686043cf650c521798a450da9e71fba35b5
Alan Ho <uqaho4@uq.edu.au>
2018-11-22 09:36:36 +1000

    new test

---
+++ R/process_accelerometry.R
@@ -0,0 +1,15 @@
+#' @title process_accelerometry
+#'
+#' @description Processes accelerometry data from .agd files
+#' @param n Number of files
+#' @export process_accelerometry
+#' @examples \dontrun{
+#' hello()
+#' }
+
+process_accelerometry = function(df){
+
+  print(df %>% slice(1:10))
+
+}
+

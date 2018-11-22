87de7dc76e3514f60629f9ee3b7731f8b6686ffa
Alan Ho <uqaho4@uq.edu.au>
2018-11-22 14:54:35 +1000

    fixing more bugs with plotting

--- R/run_grids.R
+++ R/run_grids.R
@@ -18,6 +18,154 @@
 #' hello()
 #' }
 
+hump_colours = c('1' = '#76c100', '6' = '#fc0f00')
+
+colors = list(
+  'AIT' = c('#FF6D68', '#E92323'),
+  'MIT' = c('#68B7FF', '#0037F6'),
+  'LIT' = c('#D2FFBA', '#58F520')
+)
+
+
+group_parameters = list(
+
+  'ALL' = list(
+    'colours' = "Exercise",
+    'has.x' = F,
+    'has.y' = T,
+    'lim' = 0.1
+  ),
+
+
+  'AIT' = list(
+    'colours' = "#E92323",
+    'has.x' = F,
+    'has.y' = F,
+    'lim' = 0.1
+  ),
+
+  'MIT' = list(
+    'colours' = "#0037F6",
+    'has.x' = T,
+    'has.y' = T,
+    'lim' = 0.3
+  ),
+
+  'LIT' = list(
+    'colours' = "#58F520",
+    'has.x' = T,
+    'has.y' = F,
+    'lim' = 0.1
+  )
+)
+
+
+blood_parameters = list(
+
+  'hgh' = list(
+    'title' = '(ng/ml)',
+    'limit' = 5,
+    'annot' = 3
+  ),
+
+  'betahydroxy' = list(
+    'title' = '(ng/ml)',
+    'limit' = 110,
+    'annot' = 20,
+    'percent' = 300
+  ),
+
+  'cortisol' = list(
+    'title' = '(ug/L)',
+    'limit' = 180,
+    'annot' = 60
+  ),
+
+  'gh' = list(
+    'title' = '(ug/L)',
+    'limit' = 4000,
+    'annot' = 1000
+  ),
+
+  'insulin' = list(
+    'title' = '(pmol/L)',
+    'limit' = 160,
+    'annot' = 60
+  ),
+
+  'insulin' = list(
+    'title' = '(pmol/L)',
+    'limit' = 160,
+    'annot' = 60
+  ),
+
+  'leptin' = list(
+    'title' = '(pg/ml)',
+    'limit' = 20000,
+    'annot' = 5000
+  ),
+
+  'bdnf' = list(
+    'title' = '(pg/ml)',
+    'limit' = 10000,
+    'annot' = 2000
+  ),
+
+  'prolactin' = '(ng/ml)',
+
+  'igf1' = '(ng/ml)'
+)
+
+# y limits lookup
+limit_dict = c(
+  'hgh' = 5,
+  'betahydroxy' = 70,
+  'cortisol' = 180,
+  'gh' = 4000,
+  'insulin' = 160,
+  'leptin' = 20000,
+  'prolactin' = 20,
+  'bdnf' = 8000
+)
+
+
+ytitle = c(
+  'hgh' = '(ng/ml)', # check this set of units
+  'betahydroxy' = '(ng/ml)',
+  'cortisol' = '(ug/L)',
+  'gh' = '(ng/ml)',
+  'insulin' = '(pmol/L)',
+  'ketone' = '(ng/uL)',
+  'leptin' = '(pg/ml)',
+  'prolactin' = '(ng/ml)', # also check this
+  'bdnf' = '(pg/ml)',
+  'igf1' = '(ng/ml)'
+)
+
+
+cognitive_limits = list(
+
+  'paltea' = 60,
+  'paltea8' = 50,
+  'hgh' = 5,
+  'betahydroxy' = 70,
+  'cortisol' = 180,
+  'gh' = 4000,
+  'insulin' = 160,
+  'leptin' = 20000,
+  'prolactin' = 20,
+  'bdnf' = 8000,
+  'left_CA1' = 0.1,
+  'left_CA23/DG' = 0.2,
+  'left_SUB' = 0.3,
+  'left_Whole_hipp' = 0.3,
+  "Whole_hipp" = 0.5,
+  'right_CA1' = 0.1,
+  'right_CA23DG' = 0.2,
+  'right_SUB' = 0.3,
+  'SUB' = 0.4
+)
+
 
 plot_group = function(ex,
                       df,

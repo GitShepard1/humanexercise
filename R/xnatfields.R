2d3d297fd9f9d8b2033b61415cf5c4de071271b0
Alan Ho <uqaho4@uq.edu.au>
2018-11-22 12:03:17 +1000

    adding lazy data

---
+++ R/xnatfields.R
@@ -0,0 +1,9 @@
+#' @title xnatfields
+#'
+#' @description dataframe of xnat fields
+#' @name xnatfields
+#' @docType data
+#' @usage xnatfields
+#' @format a dataframe of field names
+#' @keywords datasets
+NULL

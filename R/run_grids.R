8436c875195721391b93e6296a7d852a0eabc062
Alan Ho <uqaho4@uq.edu.au>
2018-11-23 14:27:30 +1000

    adding the new run_grids

--- R/run_grids.R
+++ R/run_grids.R
@@ -11,12 +11,11 @@
 #' @import dplyr
 #' @import tibble
 #' @import tidyr
-#' @export plot_group
+#' @export run_grids
 #' @examples \dontrun{
 #' hello()
 #' }
 
-blood_expts = c("bloodMultiplexData","bloodCobasData","bloodElisasData", "bloodIgfData")
 
 hump_colours = c('1' = '#76c100', '6' = '#fc0f00')
 
@@ -165,14 +164,7 @@ cognitive_limits = list(
   'SUB' = 0.4
 )
 
-
-plot_group = function(ex,
-                      df,
-                      xvar,
-                      yvar,
-                      xlimit=c(0,110),
-                      ylimit=c(0,50),
-                      annotx){
+plot_group = function(ex, df, xvar, yvar, xlimit=c(0,110), ylimit=c(0,50), annotx, annoty){
 
   blood = str_split(xvar, '_')[[1]][1]
 
@@ -185,7 +177,7 @@ plot_group = function(ex,
               cor.coef = TRUE,
               cor.coeff.args = list(method = "pearson",
                                     label.x = annotx,
-                                    label.y = 0.3),
+                                    label.y = annoty),
               # ylim = NA,
               # xlim = NA
               ylim = ylimit,
@@ -207,3 +199,360 @@ plot_group = function(ex,
 
 }
 
+grid_scatter = function(xvar,
+                        yvar,
+                        xexperiment,
+                        yexperiment,
+                        sel_interval=0,
+                        height = 7,
+                        width = 7,
+                        output_dir = outputdir,
+                        type = 'COGNITIVE'){
+
+  if(type == 'COGNITIVE'){
+
+    data = full_join(
+      fetchdata(exercise_db,
+                experiments = yexperiment)@data %>%
+        filter(interval %in% c(1,3,6)) %>%
+        dplyr::mutate(interval = ifelse(interval == 1, 0, interval)),
+
+      fetchdata(exercise_db,
+                experiments = xexperiment
+                # omit_subjects = c('1102RC', '1110MD')
+      )@data %>%
+        filter(interval %in% c(0,3,6)),
+
+      by = c('Subject', 'Exercise', 'interval', 'Gender')
+
+    ) %>%
+      dplyr::mutate(Exercise = factor(Exercise, levels = c('AIT', 'MIT', 'LIT')))
+
+
+    data_pk = data %>%
+      group_by(interval) %>%
+      nest() %>%
+      dplyr::mutate(Exercise = 'ALL') %>%
+      bind_rows(
+
+        data %>%
+          group_by(Exercise, interval) %>%
+          nest()
+
+      ) %>%
+      filter(interval == sel_interval)
+
+
+
+
+  } else if(type == 'LEARNING'){
+
+    data = full_join(
+
+      fetchdata(exercise_db,
+                experiments = xexperiment
+                # omit_subjects = c('1102RC', '1110MD')
+      )@data %>%
+        filter(interval %in% c(0,3,6)),
+
+      (fetchdata(exercise_db,
+                 experiments = yexperiment,
+                 omit_subjects = '1128LW'
+      ) %>%
+        delta_change(start = 1, end = 6) %>%
+        reshape_delta_change())@data
+
+
+    )
+
+    data_pk = data %>%
+      group_by(interval) %>%
+      nest() %>%
+      dplyr::mutate(Exercise = 'ALL') %>%
+      bind_rows(
+
+        data %>%
+          group_by(Exercise, interval) %>%
+          nest()
+
+      ) %>%
+      filter(interval == sel_interval)
+
+  } else if(type == 'CHANGE'){
+
+
+    data = full_join(
+
+      (fetchdata(exercise_db,
+                 experiments = yexperiment
+                 # omit_subjects = c('1102RC', '1110MD')
+      ) %>%
+        delta_change(start = 0, end = 6) %>%
+        reshape_delta_change())@data,
+
+
+      (fetchdata(exercise_db,
+                 experiments = xexperiment,
+                 omit_subjects = '1128LW'
+      ) %>%
+        delta_change(start = 1, end = 6) %>%
+        reshape_delta_change())@data
+
+    )
+
+    data_pk = data %>%
+      nest() %>%
+      dplyr::mutate(Exercise = 'ALL') %>%
+      bind_rows(
+
+        data %>%
+          group_by(Exercise) %>%
+          nest()
+
+      )
+
+
+
+
+
+  }
+
+
+  # computing limits based on maximums
+  xlims = max(
+    max(data[,xvar], na.rm = T)*1.1,
+    abs(min(data[,xvar], na.rm = T)*1.1)
+  )
+
+  ylims = max(
+    max(data[,yvar], na.rm = T)*1.1,
+    abs(min(data[,yvar], na.rm = T)*1.1)
+  )
+
+  xannot = xlims/4
+  yannot = ylims/4
+
+  bloodtype = str_split(xvar,'_')[[1]][1]
+
+  if(type %in% c('LEARNING', 'COGNITIVE')){
+
+    xheader = str_c(xvar,
+                    ' at ',
+                    sel_interval,
+                    ' Month')
+    fname = str_c(type,'-',xvar,'_', yvar, '_at_', sel_interval, '.png')
+
+
+    if(str_detect(xvar, 'percent')){
+
+      xlimit = c(-1*xlims, xlims)
+      xheader = str_c(xvar,
+                      '(%)',
+                      ' at ',
+                      sel_interval,
+                      ' Month')
+
+    } else if(str_detect(xvar, 'prepost')) {
+
+      xlimit = c(-1*xlims, xlims)
+
+    } else {
+
+      xlimit = c(0, xlims)
+
+    }
+
+  } else if (type == 'CHANGE') {
+
+    xheader = str_c(xvar, ' change (baseline to 6 months)')
+
+    fname = str_c(type,'-',xvar,'_', yvar, 'LEARNINGVSDELTA.png')
+
+    if(str_detect(xvar, 'percent')){
+
+      xlimit = c(-1*xlims, xlims)
+      xheader = str_c(xvar,  '(%)',' learning (baseline to 6 months)')
+
+    } else {
+
+      xlimit = c(-1*xlims, xlims)
+
+    }
+
+
+  }
+
+  if(type == 'COGNITIVE'){
+
+    yheader = str_c(yvar, ' at ', sel_interval, ' Month')
+    ylimit = c(0, ylims)
+
+    if (sel_interval == 0 ){
+
+      yheader = str_c(yvar, ' at baseline')
+    }
+
+    print(yheader)
+
+  } else if (type %in% c('LEARNING', 'CHANGE')){
+
+    yheader = str_c(yvar, ' learning (baseline to 6 months)')
+    ylimit = c(-1*ylims, ylims)
+
+  }
+
+  p = data_pk %>%
+    dplyr::mutate(
+
+      plots = pmap(
+
+        list(Exercise, data),
+
+        .f = plot_group,
+
+        xvar = xvar,
+        yvar = yvar,
+        xlimit = xlimit,
+        ylimit = ylimit,
+        annotx = xannot,
+        annoty = yannot
+
+      )
+
+    ) %>%
+    arrange(match(Exercise, c("ALL", "AIT", "MIT", "LIT")))
+
+
+
+  # add legend
+  plot = cowplot::plot_grid(
+
+    cowplot::plot_grid(
+
+      cowplot::get_legend(
+        filter(p, Exercise == 'ALL')$plots[[1]] +
+          theme(legend.position = 'top')
+      )
+    ),
+
+
+    cowplot::plot_grid(
+      plotlist = p$plots[1:4],
+
+      rel_widths = c(1.08,0.9)
+
+    ),
+
+    ncol = 1,
+    rel_heights = c(0.1,0.9)
+  )
+
+
+  # add padding and headings
+
+
+
+
+  # MAKING PLOTS
+  p1 <- cowplot::add_sub(plot,
+                         xheader,
+                         vpadding=grid::unit(2,"lines"),
+                         y=2,
+                         x=0.5,
+                         vjust=6,
+                         size=12)
+
+  p2 <- ggdraw() + draw_label(yheader, fontface='plain', angle = 90, size = 12)
+
+
+  # return plot
+  psave = cowplot::plot_grid(p2, cowplot::ggdraw(p1), rel_widths = c(0.03,0.97))
+
+  # return(plot_return)
+  # save plot
+
+  return(psave)
+
+
+  # ggsave(filename = fname,
+  #        plot = psave,
+  #        # width = width,
+  #        # height = height,
+  #        path = file.path('C:','Users', 'uqaho4', 'Desktop', 'Opex Report', 'Plots', 'test'))
+  #
+}
+
+
+
+run_grids = function(xvar,
+                     yvar,
+                     xexperiment,
+                     yexperiment,
+                     type,
+                     rootdir
+){
+
+  blood_types = c('pre','post', 'prepost', 'max')
+
+  if(str_detect(xexperiment, 'blood')) xvar = str_c(xvar,'_',blood_types)
+  if(str_detect(yexperiment, 'blood')) yvar = str_c(xvar,'_',blood_types)
+
+
+  p = expand.grid(
+    x = xvar,
+    y = yvar,
+    xexp = xexperiment,
+    yexp = yexperiment,
+    i = c(0,3,6),
+    t = type
+  ) %>%
+    as.tibble() %>%
+    dplyr::mutate(
+
+      x = as.character(x),
+      y = as.character(y),
+      xexp = as.character(xexp),
+      yexp = as.character(yexp),
+      i = as.integer(i),
+      t = as.character(t),
+
+      run = pmap(
+        list(x, y, xexp, yexp, i, t),
+
+        .f = try(grid_scatter)
+        # x = x,
+        # y = y,
+        # xexperiment = xexp,
+        # yexperiment = yexp,
+        # type = t
+
+      ),
+
+      paths = pmap(
+
+        list(x,y,t,i),
+
+        .f = function(x,y,t,i){
+
+          if(t == 'CHANGE'){
+
+            str_c(t,'-', x,'_',y , '.png')
+
+          } else{
+
+            str_c(t,'-', x,'_', y, '_',i,'.png')
+
+          }
+        }
+      )
+
+    ) %>%
+    unnest(paths)
+
+  savedir = file.path(rootdir,xvar)
+  if(!dir.exists(savedir)) dir.create(savedir)
+
+  pwalk(list(p$paths, p$run), ggsave, path = savedir)
+
+
+}
